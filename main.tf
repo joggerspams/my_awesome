481a135cc093981fcef9eee08cfb25d6e13c9355
joggerspams <wesbrown12@yahoo.com>
2024-01-18 19:46:12 -0600

    first

---
+++ main.tf
@@ -0,0 +1,50 @@
+# Configure the Azure provider
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+  cloud {
+    organization = "WesCorp"
+    workspaces {
+      name = "learn-terraform-azure"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+  
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team = "DevOps"
+  }
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.azurerm_virtual_network
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  tags = {
+    Titleist = "ProV1"
+  }
+}
+
+
+resource "github_repository" "example" {
+    name = "my_awesome"
+    description = "Really good"
+    visibility = "public"
+}
+
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "myTFResourceGroup"
+}
+
+variable "location" {
+    default = "SouthCentralUS"
+}
+
+variable "azurerm_virtual_network" {
+    default = "myTFVnet"
+}
